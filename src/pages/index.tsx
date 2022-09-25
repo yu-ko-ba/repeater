20752fbce03ea0231802d593dd1cc4b4eda82b0d
yu-ko-ba <yu-ko-ba@outlook.com>
2022-09-25 11:07:58 +0900

    文字数の上限を指定できるようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Container, FormControlLabel, FormGroup, Switch, TextField, ThemeProvider, Typography } from '@mui/material'
+import { Container, FormControlLabel, FormGroup, Slider, Switch, TextField, ThemeProvider, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -11,6 +11,7 @@ import repeat from '../utils/repeat'
 const Home: NextPage = () => {
   const [unprocessedtext, setUnprocessedText] = useState("")
   const [processedText, setProcessedText] = useState("")
+  const [processedTextMaxLength, setProcessedTextMaxLength] = useState(140)
   const [cramToTheMax, setCramToTheMax] = useState(false)
 
   return (
@@ -26,7 +27,11 @@ const Home: NextPage = () => {
           <TextField
             onChange={(e) => {
               setUnprocessedText(e.target.value)
-              setProcessedText(repeat(e.target.value, 140, cramToTheMax))
+              setProcessedText(repeat(
+                e.target.value,
+                processedTextMaxLength,
+                cramToTheMax
+              ))
             }}
           />
           <FormGroup>
@@ -37,7 +42,7 @@ const Home: NextPage = () => {
                     setCramToTheMax(e.target.checked)
                     setProcessedText(repeat(
                       unprocessedtext,
-                      140,
+                      processedTextMaxLength,
                       e.target.checked
                     ))
                   }}
@@ -46,6 +51,26 @@ const Home: NextPage = () => {
               label="上限いっぱいまで文字を詰め込む"
             />
           </FormGroup>
+          <Slider
+            aria-label='文字数の上限'
+            defaultValue={140}
+            onChange={(_, v) => {
+              setProcessedTextMaxLength(v as number)
+              setProcessedText(repeat(
+                unprocessedtext,
+                v as number,
+                cramToTheMax
+              ))
+            }}
+            step={35}
+            min={0}
+            max={500}
+            marks={[
+              { value: 140, label: "140" },
+              { value: 280, label: "280" }
+            ]}
+            valueLabelDisplay="auto"
+          />
           <Typography>{processedText}</Typography>
           <ButtonWithSnackbar
             buttonLabel='クリップボードにコピー'
