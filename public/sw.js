ee0901ea4a21f6f2a38b53c3702141f89e2294cd
yu-ko-ba <yu-ko-ba@outlook.com>
2022-10-17 19:08:54 +0900

    サービスワーカーが悪さをしてたから空実装に変更した

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,1 @@
-(() => {
-  self.addEventListener("fetch", (e => {
-    e.responseWith(caches.match(e.request).then((r) => {
-      return fetch(e.request)
-        .then((response) => caches.open("repeaterCache").then((c) => {
-          c.put(e.request, response.clone())
-          return response
-        })
-        .catch(() => r)
-      )
-    }))
-  }))
-})()
+// NOOP
